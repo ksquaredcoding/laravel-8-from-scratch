8f02dd5cc52b0054c1bb72755282e9004bc1e09c
ksquaredcoding <ksquaredcoding@gmail.com>
2023-03-07 10:46:11 -0700

    search and filter setup, code cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -16,22 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn () => view('posts', [
-    'posts' => Post::latest()->get(),
-    'categories' => Category::all()
-]))->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', fn (Post $post) => view('post', [
-    'post' => $post
-]));
-
-Route::get('categories/{category:slug}', fn (Category $category) => view('posts', [
-    'posts' => $category->posts,
-    'currentCategory' => $category,
-    'categories' => Category::all()
-]))->name('category');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('authors/{author:username}', fn (User $author) => view('posts', [
-    'posts' => $author->posts,
-    'categories' => Category::all()
+    'posts' => $author->posts
 ]));
