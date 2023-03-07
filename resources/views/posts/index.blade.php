96b41770141fc4c087790e591d33db2332ef3e31
ksquaredcoding <ksquaredcoding@gmail.com>
2023-03-07 15:14:20 -0700

    register a user, paginate withQueryString

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
     @include('posts._header')
+    <section class="px-6 py-8">
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
 
-    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if ($posts->count())
-        <x-posts-grid :posts="$posts" />
-
-        {{$posts->links()}}
-        @else
-        <p class="text-center">No posts yet, please check back later.</p>
-        @endif
-    </main>
+            {{$posts->links()}}
+            @else
+            <p class="text-center">No posts yet, please check back later.</p>
+            @endif
+        </main>
+    </section>
 </x-layout>
