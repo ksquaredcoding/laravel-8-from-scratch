7c40aa5c59988efa180db967a4d54e03c5fb03e7
ksquaredcoding <ksquaredcoding@gmail.com>
2023-03-09 13:57:35 -0700

    admin provider route resource implementation done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         Paginator::useTailwind();
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'johndoe';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin');
+        });
     }
 }
